ec2070fca660c62c2a957c7ef068c22af7a7ceeb
OliverKrieger <oliver.krieger76@gmail.com>
2021-11-23 22:13:32 +0000

    fix route redirect and failing home test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :campaigns
-  get 'contact/contact'
+  get 'contact', to: 'contact#contact' #redirect
   post 'request_contact', to: 'contact#request_contact'
   root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
