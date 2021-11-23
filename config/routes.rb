7399d8332557c2b99c5ed0f3d8673c5e2e5e7b8b
OliverKrieger <oliver.krieger76@gmail.com>
2021-11-23 21:34:47 +0000

    style new contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :campaigns
   get 'contact/contact'
+  post 'request_contact', to: 'contact#request_contact'
   root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
