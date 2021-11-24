a83b32827de8ae8cf37364be86f9315dac672666
OliverKrieger <oliver.krieger76@gmail.com>
2021-11-24 22:32:24 +0000

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Resources
   resources :campaigns
+
+  # Contact Page
   get 'contact', to: 'contact#contact' #redirect
   post 'request_contact', to: 'contact#request_contact'
+
+  # Home page / root where we arrive at
   root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
