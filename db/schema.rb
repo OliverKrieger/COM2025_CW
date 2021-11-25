a0549cc9606323bf2c7dd95dcd699aa7bb24d9a2
OliverKrieger <oliver.krieger76@gmail.com>
2021-11-25 22:00:49 +0000

    add correct schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,12 @@
 ActiveRecord::Schema.define(version: 2021_11_25_202440) do
 
   create_table "campaigns", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
   create_table "characters", force: :cascade do |t|
