18e28c9ba492b761af5c81cafe41c1237121dc7c
Krieger <ok00239@heron190.eps.surrey.ac.uk>
2021-12-05 21:13:49 +0000

    add extra campaign tests

--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -27,6 +27,52 @@ class CampaignsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to campaign_url(Campaign.last)
   end
 
+  test "should not save empty campaign" do
+    campaign = Campaign.new
+
+    campaign.save
+    refute campaign.valid?
+  end
+
+  test "should save only valid campaign" do
+    campaign = Campaign.new
+    user = User.new
+
+    campaign.name = "Campaign New"
+    campaign.desc = "Campaign description"
+    campaign.user_id = users(:one).id
+
+    campaign.save!
+    assert campaign.valid?
+  end
+
+  test "should not save campaign without title" do
+    campaign = Campaign.new
+
+    campaign.desc = "Campaign description"
+
+    campaign.save
+    refute campaign.valid?
+  end
+
+  test "should not save duplicate name campaign" do
+    user = User.new
+
+    campaign1 = Campaign.new
+    campaign1.name = "Campaign name"
+    campaign1.desc = "Description name"
+    campaign1.user_id = users(:one).id
+    campaign1.save
+    assert campaign1.valid?
+
+    campaign2 = Campaign.new
+    campaign2.name = "Campaign name"
+    campaign2.desc = "Description name"
+    campaign2.user_id = users(:one).id
+    campaign2.save
+    refute campaign2.valid?
+  end
+
   test "should show campaign" do
     get campaign_url(@campaign)
     assert_response :success
