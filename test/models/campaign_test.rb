fcfcd3e83e1051076a900918ccdf40c8b40fadac
Krieger <ok00239@heron170.eps.surrey.ac.uk>
2021-12-06 15:29:53 +0000

    move campaign tests to model test that are related

--- test/models/campaign_test.rb
+++ test/models/campaign_test.rb
@@ -6,6 +6,53 @@ class CampaignTest < ActiveSupport::TestCase
   # end
 
   setup do
+    @campaign = campaigns(:one)
     @user = users(:one)
+  end
+
+  test "should not save empty campaign" do
+    campaign = Campaign.new
+
+    campaign.save
+    refute campaign.valid?
+  end
+
+  test "should save only valid campaign" do
+    campaign = Campaign.new
+    user = User.new
+
+    campaign.name = "Campaign New"
+    campaign.desc = "Campaign description"
+    campaign.user_id = users(:one).id
+
+    campaign.save!
+    assert campaign.valid?
+  end
+
+  test "should not save campaign without title" do
+    campaign = Campaign.new
+
+    campaign.desc = "Campaign description"
+
+    campaign.save
+    refute campaign.valid?
+  end
+
+  test "should not save duplicate name campaign" do
+    user = User.new
+
+    campaign1 = Campaign.new
+    campaign1.name = "Campaign name"
+    campaign1.desc = "Description name"
+    campaign1.user_id = users(:one).id
+    campaign1.save
+    assert campaign1.valid?
+
+    campaign2 = Campaign.new
+    campaign2.name = "Campaign name"
+    campaign2.desc = "Description name"
+    campaign2.user_id = users(:one).id
+    campaign2.save
+    refute campaign2.valid?
   end
 end
