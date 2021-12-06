3cdbbc2122d9b2bfd271cc3ffb0e580e73ca95e4
Krieger <ok00239@heron170.eps.surrey.ac.uk>
2021-12-06 18:08:42 +0000

    assert that character is destroyed when campaign is destroyed

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -25,5 +25,32 @@ class CharacterTest < ActiveSupport::TestCase
     character.save
     assert character.valid?
   end
+
+  test 'should delete character if campaign is deleted' do
+    user = User.new
+    campaign = Campaign.new
+
+    user.email = 'bob@example.com'
+    user.password = '12345678'
+    user.save
+
+    campaign.name = "Campaign New"
+    campaign.desc = "Campaign description"
+    campaign.user_id = user.id
+    campaign.save
+
+    character = Character.new
+    character.name = "My character"
+    character.campaign = campaign
+    character.save
+
+    assert user.valid?
+    assert campaign.valid?
+    assert character.valid?
+
+    campaign.destroy
+    assert campaign.destroyed?
+    refute_equal "My character", Character.last.name
+  end
   
 end
